e58511ed8e5606e3c181f0679433d9f4ed46eca3
Markanjski <markoadzic23@gmail.com>
2024-04-08 21:00:39 +0200

    Finishet string reverse exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let textArr = text.split(""), reversed = [];
+    while (textArr.length > 0){
+        reversed.push(textArr.pop());
+    }
+    return reversed.join("");
 
 };
 
