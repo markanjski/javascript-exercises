50d12bc2b0297127b17efd541906e71eb6b19edf
Markanjski <markoadzic23@gmail.com>
2024-04-14 10:39:55 +0200

    First test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let start, end, sum = 0;
+    if (a>b) {
+        start = b;
+        end = a;
+    }else {
+        start = a;
+        end = b;
+    }
+    console.log(start, end);
+    while (start <= end){
+        sum += start;
+        start++;
+    }
+    return sum;
 
 };
 
