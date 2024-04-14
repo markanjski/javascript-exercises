613c5ee12810d115b85ba739587a8e56150e0505
Markanjski <markoadzic23@gmail.com>
2024-04-14 16:56:31 +0200

    Small edit on leapYear exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,5 @@
 const leapYears = function(year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
     if (year % 400 == 0) return true;
     else if (year % 100 == 0) return false;
     else if (year % 4 == 0) return true;
