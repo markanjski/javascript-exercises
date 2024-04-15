d95a7f295d5e469bb3949f4065fc0a6460d5e188
Markanjski <markoadzic23@gmail.com>
2024-04-15 23:18:02 +0200

    Finished Palindromes exercse

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (astr) {
+    
+    let str = astr.replace(/[^A-z^0-9]/g, "");
+  
+    return str.split("").reverse().join("").toLowerCase() === str.toLowerCase();
 };
 
 // Do not edit below this line
