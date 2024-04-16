285633f7c3de413f52fd375faa091b6bd7bb048b
Markanjski <markoadzic23@gmail.com>
2024-04-16 18:42:27 +0200

    Finished Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  let arr = [0, 1];
+  if (typeof a !== "number") count = parseInt(a);
+  else count = a;
+  if (count < 0) return "OOPS";
+  for (let i = 2; i <= count; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[count];
 };
 
 // Do not edit below this line
