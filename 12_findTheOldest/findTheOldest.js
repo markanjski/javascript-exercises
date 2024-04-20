cc64373925e885131482589369c8acd1dfa80603
Markanjski <markoadzic23@gmail.com>
2024-04-20 10:57:05 +0200

    Finished findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((acc, person, index) => {
+    let now = new Date();
+    now = now.getFullYear();
+    if (
+      (person.yearOfDeath || now) - person.yearOfBirth >
+      (acc.yearOfDeath || now) - acc.yearOfBirth
+    ) {
+      return person;
+    } else {
+      return acc;
+    }
+  }, people[0]);
 };
 
 // Do not edit below this line
